ca068219fa67cd778c3ff86250dd69f56c4dffb4
wjcawm <1592209415@qq.com>
2020-04-05 15:10:39 +0800

    代码更新

---
+++ src/views/home/livingSug.vue
@@ -0,0 +1,16 @@
+<style lang="scss" scoped>
+.livingVideo {
+  width: 1195px;
+  height: 673px;
+}
+</style>
+<template>
+  <div>
+    <video class="livingVideo"></video>
+  </div>
+</template>
+<script>
+export default {
+  name: "livingSug"
+};
+</script>
